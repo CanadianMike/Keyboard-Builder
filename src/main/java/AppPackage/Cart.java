989a618098245a3e1f1cfcc246cff3f64d0c07ed
CanadianMike <78509717+CanadianMike@users.noreply.github.com>
2022-02-02 12:31:49 -0700

    add addCart and showCart method - remove IOException

--- src/main/java/AppPackage/Cart.java
+++ src/main/java/AppPackage/Cart.java
@@ -10,30 +10,48 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Hao
  */
 public class Cart {
+    public ArrayList<String> cartItems = new ArrayList<String>();
+    String item = "";
+    Cart (String additem)
+    {
+        item = additem;
+    }
     static void initCart () throws IOException  
     {   
         //specify file for creation
-        File cartFile = new File("C:\\Users\\Hao\\Documents\\NetBeansProjects\\Keyboard-Builder\\src\\main\\java\\AppPackage\\cart.txt");
+        File cartFile = new File("src\\main\\java\\AppPackage\\cart.txt");
        if (cartFile.createNewFile())
        {System.out.println("File created");}
        else{System.out.println("File already exists");}
         System.out.println("files written!");  
     }
-    static void addCart (String cartItem) throws IOException
+    public void addCart (String cartItem) 
     {
+        cartItems.add(cartItem);
+        
         //declare file writer
-        FileWriter cartfw = new FileWriter("C:\\Users\\Hao\\Documents\\NetBeansProjects\\Keyboard-Builder\\src\\main\\java\\AppPackage\\cart.txt");
-        PrintWriter cartOutput = new PrintWriter(cartfw);
-        cartOutput.println(cartItem);
-        cartOutput.close();
-        cartfw.close();
+//        FileWriter cartfw = new FileWriter("src\\main\\java\\AppPackage\\cart.txt");
+//        cartfw.write(cartItem);
+//        PrintWriter cartOutput = new PrintWriter(cartfw);
+//        cartOutput.println(cartItem);
+//        cartOutput.close();
+//        cartfw.close();
     }
+    public void showCart ()
+    {
+        System.out.print(cartItems);
+    }
+
     //change to return string to be used in main cart method in future
 //    static void switchAddCart (String cartItem)
 //    {
